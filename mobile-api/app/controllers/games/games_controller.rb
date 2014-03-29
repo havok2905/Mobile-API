a7a037445c4f5216de62747bbbe72d3778793969
Christopher McLean <havok2905@gmail.com>
2014-03-29 03:57:18 -0400

    Added rough draft for the game's controller.

---
+++ mobile-api/app/controllers/games/games_controller.rb
@@ -0,0 +1,148 @@
+class Games::GamesController < ApplicationController
+
+  # Return all Games::Game objects in the database
+  #
+  # @type    GET
+  # @returns JSON response with all Games::Game objects
+  def get_all
+    @games = Games::Game.all
+
+    respond_to do | format |
+      format.json { render json: @games }
+    end
+  end
+
+
+  # Return a single Games::Game by a given id
+  #
+  # @type    GET
+  # @param   id
+  # @returns JSON response with a Games::Game object
+  def get_by_id
+    @game = Games::Game.where ( id: params[:id] ).last
+
+    respond_to do | format |
+      format.json { render json: @game }
+    end
+  end
+
+
+  # Return a single Games::Game object based on location
+  #
+  # @type    GET
+  # @param   latitude
+  # @param   longitude
+  # @param   range
+  # @returns JSON response with an array of Games::Game objects
+  def get_by_location
+    @center_x = @params[:latitude]
+    @center_y = @params[:longitude]
+    @radius   = @params[:range]
+
+    @games = Games::Game.select { | game |
+      in_circle_bounds? @center_x, @center_y, game.latitude, game.longitude, @radius
+    }
+
+    respond_to do | format |
+      if @game.save
+        format.json { render json: @checkpoints }
+      end
+    end
+
+  end
+
+
+  # creates a new Games::Game object
+  #
+  # @type    POST
+  # @param   game
+  # @returns JSON response with the newly created Games::Game object
+  def create
+    @game = Games::Game.new ( params[:games_game] )
+
+    respond_to do | format |
+      if @game.save
+        format.json { render json: @game }
+      else
+        format.json { render json: { errors: @game.errors.full_messages, status: 422 } }
+      end
+    end
+  end
+
+
+  # updates a Games::Game object based on id
+  #
+  # @type    PUT
+  # @param   id
+  # @param   game
+  # @returns JSON response with the newly updated Games::Game object
+  def update
+    @game = Games::Game.where ( id: params[:id] )
+
+    respond_to do | format |
+      if @game.update_attributes ( params[:games_game] )
+        format.json { render json: @game }
+      else
+        format.json { render json: { @game.errors.full_messages, status: 422 } }
+      end
+    end
+  end
+
+
+  # updates a Games::Game object based on id
+  #
+  # @type    DELETE
+  # @param   id
+  # @returns JSON response with the newly deleted Games::Game object
+  def destroy
+    @game = Games::Game.find( params[:id] )
+
+    respond_to do | format |
+      if @game.destroy
+        format.json { render json: @game }
+      else
+        format.json { render json: { @game.errors.full_messages, status: 422 } }
+      end
+    end
+  end
+
+
+  private
+
+  ## I totally ripped these formulas from online without testing them
+  ## This will probably bite me in the ass later :/
+  ## Will break these out into a map handling class later after I'm sure they work
+
+  # Return if a checkoint is within bounds of a set circle
+  #
+  # @param   latitude
+  # @param   longitude
+  # @param   range
+  # @returns boolean
+  def in_circle_bounds? ( @center_x, @center_y, @x, @y, @radius )
+    ( ( @x - @center_x )**2 ) + ( ( @y - @center_y )**2 ) < @radius**2
+  end
+
+
+  # Return if a checkoint is on the border of a set circle
+  #
+  # @param   latitude
+  # @param   longitude
+  # @param   range
+  # @returns boolean
+  def on_circle_bounds? ( @center_x, @center_y, @x, @y, @radius )
+    ( ( @x - @center_x )**2 ) + ( ( @y - @center_y )**2 ) == @radius**2
+  end
+
+
+  # Return if a checkoint is out of the of a set circle
+  #
+  # @param   latitude
+  # @param   longitude
+  # @param   range
+  # @returns boolean
+  def out_circle_bounds? ( @center_x, @center_y, @x, @y, @radius )
+    ( ( @x - @center_x )**2 ) + ( ( @y - @center_y )**2 ) > @radius**2
+  end
+
+end
--- mobile-api/app/models/games/game.rb
+++ mobile-api/app/models/games/game.rb
@@ -1,3 +1,4 @@
 class Games::Game < ModelBase
   has_many :checkpoints, class_name: 'Checkpoints::Checkpoint'
+
 end
