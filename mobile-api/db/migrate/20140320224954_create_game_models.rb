e2fd122018601f64c837857daf6fd016cc7f0852
Christopher McLean <havok2905@gmail.com>
2014-03-30 18:33:43 -0400

    added factories

--- mobile-api/db/migrate/20140320224954_create_game_models.rb
+++ mobile-api/db/migrate/20140320224954_create_game_models.rb
@@ -3,6 +3,8 @@ class CreateGameModels < ActiveRecord::Migration
     create_table :games do |t|
       t.string :name
       t.text   :description
+      t.string :latitude
+      t.string :longitude
       t.timestamps
     end
   end
