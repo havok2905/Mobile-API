346978e8fe50078a463f288ef25d60989785ec67
Christopher McLean <havok2905@gmail.com>
2014-03-30 21:44:54 -0400

    added skeleton for game controller test

---
+++ mobile-api/spec/controllers/game_controller_spec.rb
@@ -0,0 +1,23 @@
+require '../spec_helper.rb'
+
+describe Games::GameController do
+
+  describe 'GET #index' do
+  end
+
+  describe 'GET #show' do
+  end
+
+  describe 'GET #by_location' do
+  end
+
+  describe 'POST #create' do
+  end
+
+  describe 'PUT #update' do
+  end
+
+  describe 'DELETE #destroy' do
+  end
+
+end
