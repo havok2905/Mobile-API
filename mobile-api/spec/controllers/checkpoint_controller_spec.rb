eedc36a41188cb5d9146bfb46b60b2a5c23f534e
Christopher McLean <havok2905@gmail.com>
2014-04-12 19:22:45 -0400

    more testing

--- mobile-api/spec/controllers/checkpoint_controller_spec.rb
+++ mobile-api/spec/controllers/checkpoint_controller_spec.rb
@@ -22,10 +22,18 @@ describe Checkpoints::CheckpointController, :type => :controller do
 
   before :each do
     request.env["HTTP_ACCEPT"] = 'application/json'
+
     @game = FactoryGirl.create(:game)
+
     @checkpoint_one = FactoryGirl.create(:checkpoint)
+    @checkpoint_one.update_column(:game_id, 1)
+
     @checkpoint_two = FactoryGirl.create(:checkpoint)
+    @checkpoint_two.update_column(:game_id, 1)
+
     @checkpoints = [@checkpoint_one, @checkpoint_two]
+
+    @association = FactoryGirl.create(:association, checkpoints: @checkpoints)
   end
 
 
@@ -48,11 +56,40 @@ describe Checkpoints::CheckpointController, :type => :controller do
   end
 
 
-  describe 'GET #get_checkpoint_by_game' do
+  describe 'GET #get_checkpoints_by_game' do
+    it "Responds with valid checkpoints" do
+      get :get_checkpoints_by_game, game_id: @game.id
+      @checkpoint_response = JSON.parse(response.body)
+      response_checkpoint_comparison @checkpoint_one, @checkpoint_response.first
+      response_checkpoint_comparison @checkpoint_two, @checkpoint_response.last
+    end
+  end
+
+
+  describe 'GET #get_checkpoints_by_location' do
+    it "Responds with valid checkpoint" do
+      get :get_checkpoints_by_location, latitude: 28.600000, longitude: -81.339235
+      @checkpoint_response = JSON.parse(response.body)
+      response_checkpoint_comparison @checkpoint_one, @checkpoint_response.first
+      response_checkpoint_comparison @checkpoint_two, @checkpoint_response.last
+    end
+  end
+
+
+  describe 'GET #get_possible_next_checkpoints' do
+    it "Responds with valid checkpoints" do
+      get :get_possible_next_checkpoints, id: @checkpoint_one.id
+      @checkpoint_response = JSON.parse(response.body)
+      response_checkpoint_comparison @checkpoint_two, @checkpoint_response.first
+    end
+  end
+
+
+  describe 'GET #get_previous_checkpoint' do
     it "Responds with a valid checkpoint" do
-      get :show, id: @checkpoint_one.id
-      @checkpoint_response = JSON.parse(response.body).first
-      response_checkpoint_comparison @checkpoint_one, @checkpoint_response
+      get :get_previous_checkpoint, game_id: 1
+      @checkpoint_response = JSON.parse(response.body)
+      response_checkpoint_comparison @checkpoint_two, @checkpoint_response.first
     end
   end
 
