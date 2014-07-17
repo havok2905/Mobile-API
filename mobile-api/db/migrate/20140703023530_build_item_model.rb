622c6cb6ea65cb4d538dc761a2b59e04504e626a
Christopher McLean <havok2905@gmail.com>
2014-07-16 21:34:54 -0400

    added migrations for sub items

--- mobile-api/db/migrate/20140703023530_build_item_model.rb
+++ mobile-api/db/migrate/20140703023530_build_item_model.rb
@@ -10,9 +10,32 @@ class BuildItemModel < ActiveRecord::Migration
     end
     add_index :items, :checkpoint_id
     add_index :items, :checkpoint_association_id
+
+    create_table :story_items do |t|
+      t.boolean :priority
+    end
+
+    create_table :health_items do |t|
+      t.integer :boost
+      t.boolean :can_self
+      t.boolean :can_target
+      t.integer :limit
+    end
+
+    create_table :weapon_items do |t|
+      t.integer :damage
+      t.boolean :splash
+      t.boolean :radius
+      t.integer :integrity
+      t.boolean :timed
+      t.integer :time
+    end
   end
 
   def down
     drop_table :items
+    drop_table :story_items
+    drop_table :health_items
+    drop_table :weapon_items
   end
 end
