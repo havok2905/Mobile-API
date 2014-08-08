c4a3344b64915102b816b4a78aff6295298adebd
Christopher McLean <havok2905@gmail.com>
2014-08-08 15:06:10 -0400

    added items controller

---
+++ mobile-api/app/controllers/items/item_controller.rb
@@ -0,0 +1,83 @@
+class Items::ItemController < ApplicationController
+
+  # Return all Items::Item objects in the database
+  #
+  # @type    GET
+  # @returns JSON response with all Items::Item objects and children
+  def index
+    @items = Items::Item.all
+
+    respond_to do | format |
+      format.json { render json: @items }
+    end
+  end
+
+
+  # Return a single Items::Item by a given id
+  #
+  # @type    GET
+  # @param   id
+  # @returns JSON response with a Items::Item object
+  def show
+    @item = Items::Item.where( id:params[:id] ).all
+
+    respond_to do | format |
+      format.json { render json: @item }
+    end
+  end
+
+
+  # creates a new Items::Item object
+  #
+  # @type    POST
+  # @param   item
+  # @returns JSON response with the newly created Items::Item object
+  def create
+    @effect = Items::Item.new ( params[:items_item] )
+
+    respond_to do | format |
+      if @item.save
+        format.json { render json: @item }
+      else
+        format.json { render json: { errors: @item.errors.full_messages, status: 422 } }
+      end
+    end
+  end
+
+
+  # updates a Items::Item object based on id
+  #
+  # @type    PUT
+  # @param   id
+  # @param   item
+  # @returns JSON response with the newly updated Items::Item object
+  def update
+    @item = Items::Item.where( id:params[:id] )
+
+    respond_to do | format |
+      if @item.update_attributes ( params[:items_item] )
+        format.json { render json: @item }
+      else
+        format.json { render json: { errors: @item.errors.full_messages, status: 422 } }
+      end
+    end
+  end
+
+
+  # updates a Items::Item object based on id
+  #
+  # @type    DELETE
+  # @param   id
+  # @returns JSON response with the newly deleted Items::Item object
+  def destroy
+    @item = Items::Item.find( params[:id] )
+
+    respond_to do | format |
+      if @item.destroy
+        format.json { render json: @item }
+      else
+        format.json { render json: { errors: @item.errors.full_messages, status: 422 } }
+      end
+    end
+  end
+end
